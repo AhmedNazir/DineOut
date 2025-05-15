66d2d83660f87f63758d73a418c63230b1bc7289
ahmednazir <ahmednazir333@gmail.com>
2025-05-15 14:41:54 +0600

    'publish'

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
     return (
         <>
-            <h1 class="text-3xl font-bold underline text-center">
+            <h1 class="text-3xl font-bold underline text-center text-red-600 bg-yellow-300">
                 Hello world!
             </h1>
         </>
